e2916a8a98c7915325956599f90ef5b58ad28826
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-11-22 17:20:35 -0500

    Test

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
-import HomePage from './pages/HomePage';
-import GLTFViewer from './components/GLTFViewer';
+import logo from "./logo.svg";
+import styles from "./App.module.css";
+import HomePage from "./pages/HomePage";
+import GLTFViewer from "./components/GLTFViewer";
 
 function App() {
   return (
     <div class={styles.App}>
-      <GLTFViewer modelPath="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf" />
+      <GLTFViewer modelPath="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/2CylinderEngine/glTF/2CylinderEngine.gltf" />
       <header class={styles.header}>
         <HomePage />
       </header>
