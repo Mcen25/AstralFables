3dc33279c84482d19551f7d26534e8682ba6b915
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-11-23 14:44:40 -0500

    Homepage setup

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,7 +6,9 @@ import GLTFViewer from "./components/GLTFViewer";
 function App() {
   return (
     <div class={styles.App}>
-      <GLTFViewer modelPath="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/2CylinderEngine/glTF/2CylinderEngine.gltf" />
+      {/* <GLTFViewer modelPath="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/refs/heads/main/2.0/2CylinderEngine/glTF/2CylinderEngine.gltf" /> */}
+      {/* <GLTFViewer modelPath="https://github.com/Mcen25/VR-Assets-Dev/blob/main/Earth/Earth.glb" /> */}
+      {/* <GLTFViewer modelPath="https://raw.githubusercontent.com/Mcen25/VR-Assets-Dev/refs/heads/main/Earth%20by%20Denis%20Cliofas/scene.gltf" /> */}
       <header class={styles.header}>
         <HomePage />
       </header>
