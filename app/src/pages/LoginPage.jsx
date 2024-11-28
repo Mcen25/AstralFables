61786d60c29f4b393f9f9e101bb97d89d8cde895
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-11-27 21:39:29 -0500

    login page changes

--- app/src/pages/LoginPage.jsx
+++ app/src/pages/LoginPage.jsx
@@ -22,19 +22,42 @@ export default function LoginPage() {
     <div className={styles.root}>
       <Navbar />
       <div className={styles.columnContainer}>
-        <div className={styles.loginContainer}>
-          <button className={styles.loginButton} onClick={handleGoogleLogin}>
-            <i className="fab fa-google"></i>
-          </button>
-          <button className={styles.loginButton} onClick={handleMicrosoftLogin}>
-            <i className="fab fa-microsoft"></i>
-          </button>
-          <button className={styles.loginButton} onClick={handleGitHubLogin}>
-            <i className="fab fa-github"></i>
-          </button>
-          <button className={styles.loginButton} onClick={handleDiscordLogin}>
-            <i className="fab fa-discord"></i>
-          </button>
+        <div className={styles.section}>
+          <div className={styles.loginSection}>
+            <h2>Login</h2>
+
+            <h3>Email</h3>
+            <input type="text" placeholder="Enter your email" />
+            <h3>Password</h3>
+            <input type="password" placeholder="Enter your password" />
+          </div>
+          <button className={styles.loginMainButton}>LOGIN</button>
+          <div className={styles.horizontalLineContainer}>
+            <hr className={styles.horizontalLine} />
+            <span className={styles.horizontalLineText}>or</span>
+            <hr className={styles.horizontalLine} />
+          </div>
+          <div className={styles.sectionRow}>
+            <button className={styles.loginButton} onClick={handleGoogleLogin}>
+              <i className="fab fa-google"></i>
+            </button>
+            <button
+              className={styles.loginButton}
+              onClick={handleMicrosoftLogin}
+            >
+              <i className="fab fa-microsoft"></i>
+            </button>
+            <button className={styles.loginButton} onClick={handleGitHubLogin}>
+              <i className="fab fa-github"></i>
+            </button>
+            <button className={styles.loginButton} onClick={handleDiscordLogin}>
+              <i className="fab fa-discord"></i>
+            </button>
+          </div>
+        </div>
+        <div className={styles.section}>
+          <h2>Section 1</h2>
+          <p>Content for the first section.</p>
         </div>
       </div>
     </div>
