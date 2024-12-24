9e9a77bf03c8f83db078a12eb1d68295379c0b10
Matthew En <86386184+Mcen25@users.noreply.github.com>
2024-12-24 18:02:58 +0700

    Update users.service.spec.ts

--- app/server/src/users/users.service.spec.ts
+++ app/server/src/users/users.service.spec.ts
@@ -1,18 +1,31 @@
+// e2e/users.service.spec.ts
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Users } from './entities/users.entity';
 
 describe('UsersService', () => {
   let service: UsersService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'mongodb',
+          url: process.env.MongoDB_URI || 'mongodb://localhost:27017/testdb',
+          entities: [Users],
+          synchronize: false,
+        }),
+        TypeOrmModule.forFeature([Users]),
+      ],
       providers: [UsersService],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should connect and findAll users', async () => {
+    const result = await service.findAll();
+    expect(result).toBeDefined();
   });
 });
